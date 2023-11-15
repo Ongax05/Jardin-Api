a2e3af0f015e674d66cab16065817ba4085497c2
Jhosep <jhoseppulido1129@gmail.com>
2023-11-15 17:17:05 -0500

    feat(Aplication): :sparkles: Interfaces & Repositories linked to UnitOfWork

--- Aplication/UnitOfWork/UnitOfWork.cs
+++ Aplication/UnitOfWork/UnitOfWork.cs
@@ -1,24 +1,26 @@
 using Aplication.Repository;
+using Domain.Entities;
 using Domain.Interfaces;
 using Persistency;
+
 namespace Aplication.UnitOfWork;
+
 public class UnitOfWork : IUnitOfWork, IDisposable
 {
     private readonly ApiDbContext _context;
     private IRolRepository _roles;
     private IUserRepository _users;
+
     public UnitOfWork(ApiDbContext context)
     {
         _context = context;
     }
+
     public IRolRepository Roles
     {
         get
         {
-            if (_roles == null)
-            {
-                _roles = new RolRepository(_context);
-            }
+            _roles ??= new RolRepository(_context);
             return _roles;
         }
     }
@@ -27,13 +29,75 @@ public class UnitOfWork : IUnitOfWork, IDisposable
     {
         get
         {
-            if (_users == null)
-            {
-                _users = new UserRepository(_context);
-            }
+            _users ??= new UserRepository(_context);
             return _users;
         }
     }
+
+    private ICliente _Clientes;
+    public ICliente Clientes
+    {
+        get
+        {
+            _Clientes ??= new ClienteRepository(_context);
+            return _Clientes;
+        }
+    }
+    private IEmpleado _Empleados;
+    public IEmpleado Empleados
+    {
+        get
+        {
+            _Empleados ??= new EmpleadoRepository(_context);
+            return _Empleados;
+        }
+    }
+    private IGama_Producto _Gamas_Productos;
+    public IGama_Producto Gamas_Productos
+    {
+        get
+        {
+            _Gamas_Productos ??= new Gama_ProductoRepository(_context);
+            return _Gamas_Productos;
+        }
+    }
+    private IOficina _Oficinas;
+    public IOficina Oficinas
+    {
+        get
+        {
+            _Oficinas ??= new OficinaRepository(_context);
+            return _Oficinas;
+        }
+    }
+    private IPago _Pagos;
+    public IPago Pagos
+    {
+        get
+        {
+            _Pagos ??= new PagoRepository(_context);
+            return _Pagos;
+        }
+    }
+    private IPedido _Pedidos;
+    public IPedido Pedidos
+    {
+        get
+        {
+            _Pedidos ??= new PedidoRepository(_context);
+            return _Pedidos;
+        }
+    }
+    private IProducto _Productos;
+    public IProducto Productos
+    {
+        get
+        {
+            _Productos ??= new ProductoRepository(_context);
+            return _Productos;
+        }
+    }
+
     public async Task<int> SaveAsync()
     {
         return await _context.SaveChangesAsync();
--- Domain/Interfaces/IUnitOfWork.cs
+++ Domain/Interfaces/IUnitOfWork.cs
@@ -4,5 +4,12 @@ public interface IUnitOfWork
 {
     IRolRepository Roles { get; }
     IUserRepository Users { get; }
+    ICliente Clientes { get; }
+    IEmpleado Empleados { get; }
+    IGama_Producto Gamas_Productos { get; }
+    IOficina Oficinas { get; }
+    IPago Pagos { get; }
+    IPedido Pedidos { get; }
+    IProducto Productos { get; }
     Task<int> SaveAsync();
 }
